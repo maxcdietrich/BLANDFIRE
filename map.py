faa271f1823e6f85eeba363469430e09de47ab6e
Max Dietrich <maxcdietrich@gmail.com>
2019-11-14 19:37:15 -0500

    Add methods to Map and Tile.  Generate random map
    Add methods to Map class in map.py to allow the object to be written to
    text tiles in JSON format and to construct map objects from JSON files.
    
    Add __str__ methods to Map and Tile.  print(Map) still contains bug
    after object is printed.
    
    Add write_map.py to create random map object and write to JSON .txt file
    .  Make compatible with data sets once found.

--- map.py
+++ map.py
@@ -4,6 +4,8 @@ Create a map class and its subclasses
 @Authors: Max Dietrich
 """
 
+import json
+
 class Map:
     """
     Store information for the entire map
@@ -14,8 +16,44 @@ class Map:
         """
         self.tile_dict = tile_dict
 
+    def __str__(self):
+        """
+        Will print map properly but then throw error.  could catch with exception if it becomes a problem
+        """
+        for key in self.tile_dict:
+            print(key,'\n',self.tile_dict[key],'\n')
+
+    def toJSONs(self):
+        """
+        returns a json formatted string of Map
+        """
+        return json.dumps(self,default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    def toJSON(self, filename):
+        """
+        returns a json formatted .txt file of Map
+        """
+        with open('{}.txt'.format(filename), 'w') as outfile:
+            json.dump(self, outfile, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
+    def fromJSON(self, filename):
+        """
+        returns a Map object from a properly formatted json .txt file.  The target file must be encoded in the same format as a map object.
+
+        Inverse of toJSON
+        """
+        self.tile_dict={}
+        with open('{}.txt'.format(filename), 'r') as infile:
+            top_layer = json.load(infile)['tile_dict']
+            for key in top_layer:
+                tile_data = top_layer[key]
+                tile = Tile(**tile_data)
+                self.tile_dict[key] = tile
+        return self.tile_dict
+
+
 class Tile:
-    def __init__(self, is_burning=False, flammability=0, fuel=0, weather=None, rain=None, elevation=0):
+    def __init__(self, is_burning=False, flammability=0, fuel=0, wind=None, elevation=0):
         """
         Initiate the information for a tile
         """
@@ -23,5 +61,11 @@ class Tile:
         self.flammability = flammability #0-100
         self.fuel = fuel #0-100
         self.wind = wind #should be a vector quantity
-        self.rain = rain #IMPLEMENT
-        self.elevation = elevation #IMPLEMENT (Scalar? Gradient?)
+        self.elevation = elevation
+
+    def __str__(self):
+        burning = 'is_burning ='+str(self.is_burning)+'\n'
+        flammability = 'flammability ='+str(self.flammability,)+'\n'
+        fuel = "fuel ="+str(self.fuel)+'\n'
+        elevation = "elevation ="+str(self.elevation)
+        return burning+flammability+fuel+elevation
---
+++ write_map.py
@@ -0,0 +1,38 @@
+import json
+from random import randint
+import map
+import math
+
+south_west_corner = (37.5,-120) #coordinate pair
+north_east_corner = (37.8,-119.5) #coordinate pair
+x_length = 44190 #east-west length [m] ignore earth curvature
+y_length = 33300 #north-south length [m]
+# x_length = 90 #Test values to make small map
+# y_length = 90 #Test values to make small map
+tile_size = 30 #GIS tile 30m x 30m
+
+
+def make_random_map():
+    """
+    Define a random data set for testing the model
+    """
+    random_map = map.Map()
+    for x in range(x_length//tile_size):
+        for y in range(y_length//tile_size):
+            tile = map.Tile()
+            tile.is_burning = False
+            tile.flammability = randint(25,75)
+            tile.fuel = randint(25,75)
+            tile.wind = (1, 1) #speed, direction
+            tile.elevation = math.sin(x/(100*math.pi))*5 #1D sine wave
+            random_map.tile_dict[str((x,y))] = tile #key is a str because json does not take tuples as keys
+    return random_map
+
+def write_random_map(filename):
+    """
+    Create a random map and write it to a text file in a JSON format
+    """
+    random_map = make_random_map()
+    random_map.toJSON(filename)
+
+write_random_map('test_map')
