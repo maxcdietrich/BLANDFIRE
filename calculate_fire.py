52300fbb42c39fc5a75b8bee07bd4eb6eda90e16
Max Dietrich <maxcdietrich@gmail.com>
2019-11-19 15:49:33 -0500

    Render fire spread

--- calculate_fire.py
+++ calculate_fire.py
@@ -8,13 +8,12 @@ with the new result
 """
 from random import randint
 import map
+import render
 
 
 def catch_on_fire(center, test_map):
     """
     Calculate the probabilty for a cell on fire to light its adjacent cells on fire
-
-    Not set up to handle edges and other edge cases
     """
     up_left = (center[0]-1, center[1]+1)
     up = (center[0], center[1]+1)
@@ -26,17 +25,20 @@ def catch_on_fire(center, test_map):
     down_right = (center[0]+1, center[1]-1)
     cells_to_check = [up_left, up, up_right, left, right, down_left, down, down_right]
     new_burning_cells = []
-    for cell in cells_to_check:
-        roll = randint(0, 100) #create a random roll to check for fire spread
-        const_factor = 0.58
-        wind_factor = 1
-        flam_factor = 1
-        fuel_factor = 1
-        elevation_factor = 1
-        ignition_probability = const_factor*wind_factor*flam_factor*fuel_factor*elevation_factor #create the probability of the adjacent cell catching on fire
-        if roll < ignition_probability*100: #compare the roll to the ignition_probability
-            test_map.tile_dict[cell].is_burning = True #the adjacent cell catches on fire
-            new_burning_cells.append(cell)
+    try: #Will try to check fire spread unless the target cell is not in the map
+        for cell in cells_to_check:
+            roll = randint(0, 100) #create a random roll to check for fire spread
+            const_factor = 0.35
+            wind_factor = 1
+            flam_factor = test_map.tile_dict[cell].flammability / 100
+            fuel_factor = 1
+            elevation_factor = 1
+            ignition_probability = const_factor*wind_factor*flam_factor*fuel_factor*elevation_factor #create the probability of the adjacent cell catching on fire
+            if roll < ignition_probability*100: #compare the roll to the ignition_probability
+                test_map.tile_dict[cell].is_burning = True #the adjacent cell catches on fire
+                new_burning_cells.append(cell)
+    except KeyError:
+        pass
     return new_burning_cells
 
 def put_out(center, test_map):
@@ -56,7 +58,7 @@ def put_out(center, test_map):
 
 
 
-def calculate_fire(start_tick, tick_limit, previous_burning_cells, previous_extinguished_cells, test_map):
+def calculate_fire(start_tick, tick_limit, previous_burning_cells, previous_extinguished_cells, test_map, view_object):
     """
     Acts as the controller for the program
 
@@ -73,15 +75,21 @@ def calculate_fire(start_tick, tick_limit, previous_burning_cells, previous_exti
         current_extinguished_cells = set(previous_extinguished_cells) #set the previous state to be the current state
         for cell in previous_burning_cells:
             assert type(cell) == tuple
-            new_burning_cells = set(catch_on_fire(cell, test_map))
-            current_burning_cells |= new_burning_cells
+            additional_burning_cells = set(catch_on_fire(cell, test_map))
+            current_burning_cells |= additional_burning_cells #Union
             if put_out(cell, test_map): #improper use of methods with sets
                 current_burning_cells.remove(cell) #remove cells that are extinguished
                 current_extinguished_cells.add(cell) #add extinguised cells to this set
         current_burning_cells = sorted(current_burning_cells) #sort the lists so they are in a known order
+        if len(current_burning_cells) == 0:
+            print("The fire is out!")
+            return
         current_extinguished_cells = sorted(current_extinguished_cells)
+        new_burning_cells = list(set(current_burning_cells) - set(previous_burning_cells))
+        new_extinguished_cells = list(set(current_extinguished_cells) - set(previous_extinguished_cells))
         start_tick = start_tick + 1
-        calculate_fire(start_tick, tick_limit, current_burning_cells, current_extinguished_cells, test_map) #run with the current state as the previous state next iteration
+        view_object.update_render(new_burning_cells, new_extinguished_cells)
+        calculate_fire(start_tick, tick_limit, current_burning_cells, current_extinguished_cells, test_map, view_object) #run with the current state as the previous state next iteration
 
 
 def run_model(tick_limit):
@@ -92,8 +100,13 @@ def run_model(tick_limit):
     #need to add view functions here
     test_map = map.Map()
     test_map.fromJSON('test_map')
-    burning_cells = [(400,500)]
-    extinguished_cells = []
-    calculate_fire(0, tick_limit, burning_cells, extinguished_cells, test_map) #DO NOT PASS IN map AS A PARAMETER
 
-run_model(10)
+    last_key = max(test_map.tile_dict)
+    view = render.View(last_key[0], last_key[1])
+    view.init_render(test_map)
+
+    burning_cells = [(300,300)]
+    extinguished_cells = []
+    calculate_fire(0, tick_limit, burning_cells, extinguished_cells, test_map, view) #DO NOT PASS IN map AS A PARAMETER
+
+run_model(1000)
--- render.py
+++ render.py
@@ -20,53 +20,41 @@ class View:
         """
         self.size = (width, height)
         self.screen = pygame.display.set_mode((width, height))
+        self.rect_dict = {}
 
     def init_render(self, seed_map):
         """
         Render each tile as a green pixel prior to any burning
         """
         self.screen.fill((255, 255, 255))
-        rect_dict = {}
         for coordinate in seed_map.tile_dict:
             rectangle = pygame.Rect((coordinate[0], coordinate[1]), (1, 1))
-            rect_dict[coordinate] = rectangle
+            self.rect_dict[coordinate] = rectangle
             pygame.draw.rect(self.screen, (34, 139, 34), rectangle)
         pygame.display.flip()
         time.sleep(0.01)
 
-    def make_burning(self):
+    def make_burning(self, burn_update):
         """
         Update the color of each rectangle that is newly on fire
         """
-        pass
+        for cell in burn_update:
+            rectangle = self.rect_dict[cell]
+            pygame.draw.rect(self.screen, (255, 0, 0), rectangle)
+        pygame.display.flip()
 
-    def make_extinguished(self):
+    def make_extinguished(self, extinguish_update):
         """
         Update the color of each rectangle that is newly extinguished
         """
-        pass
+        for cell in extinguish_update:
+            rectangle = self.rect_dict[cell]
+            pygame.draw.rect(self.screen, (0, 0, 0), rectangle)
+        pygame.display.flip()
 
-    def update_render(self):
+    def update_render(self, burn_update, extinguish_update):
         """
         Cumulative updating method
         """
-        pass
-
-def draw_model():
-    """
-    Run the view functions.  Likely will be moved into calculate_fire.
-    """
-    sim_map = map.Map()
-    sim_map.fromJSON('test_map')
-    last_key = max(sim_map.tile_dict)
-    view = View(last_key[0], last_key[1])
-    view.init_render(sim_map)
-    print('done')
-    while True:
-        view.update_render()
-
-
-
-
-
-draw_model()
+        self.make_burning(burn_update)
+        self.make_extinguished(extinguish_update)
