b4fe26c82bebc4e544e311fca2c62fbe2a027903
Max Dietrich <maxcdietrich@gmail.com>
2019-12-05 18:09:41 -0500

    Add integration of get_wind.py
    Memory error still occurs in regrid() of get_wind.py when scaling the
    wind data to fit the simulation map.
    
    Added functions for making data informed maps in write_map.py.

---
+++ new_get_wind.py
@@ -0,0 +1,220 @@
+"""
+pip install pyshp
+"""
+
+import shapefile
+
+def remap_interval(val,
+    input_interval_start,
+    input_interval_end,
+    output_interval_start,
+    output_interval_end):
+    """Remap a value from one interval to another.
+
+    Given an input value in the interval [input_interval_start,
+    input_interval_end], return an output value scaled to fall within
+    the output interval [output_interval_start, output_interval_end].
+
+    Args:
+    val: the value to remap
+    input_interval_start: the start of the interval that contains all
+    possible values for val
+    input_interval_end: the end of the interval that contains all possible
+    values for val
+    output_interval_start: the start of the interval that contains all
+    possible output values
+    output_interval_end: the end of the interval that contains all possible
+    output values
+
+    Returns:
+    The value remapped from the input to the output interval
+
+    Examples:
+    >>> remap_interval(0.5, 0, 1, 0, 10)
+    5.0
+    >>> remap_interval(5, 4, 6, 0, 2)
+    1.0
+    >>> remap_interval(5, 4, 6, 1, 2)
+    1.5
+    >>> remap_interval(7, 3, 8, 0, 1)
+    0.8
+    """
+    # Assumes that input_interval_end > input_interval_start
+    # and output_interval_end > output_interval_start
+
+    diff1 = input_interval_end-input_interval_start
+    diff2 = output_interval_end-output_interval_start
+
+    # Finds the variation in range size as a ratio
+    ratio = diff2/diff1
+
+    return output_interval_start + ratio*(val-input_interval_start)
+
+
+
+myshp = open("SHAPEFILES/HOUR1/yosemite_landscape_12-03-2019_0900_120m.shp", "rb")
+mydbf = open("SHAPEFILES/HOUR1/yosemite_landscape_12-03-2019_0900_120m.dbf", "rb")
+
+
+# Taken by converting UTM Zone 11 coordinates on https://www.ngs.noaa.gov/NCAT/
+# These values specific to .shp files with preface "final_shape"
+west_lon = -119.9940268086
+east_lon = -119.4842024547
+
+south_lat = 37.4548996589
+north_lat = 37.8276271233
+
+
+class Wind:
+    def __init__(self, myshp, mydbf, west_lon, east_lon, south_lat, north_lat):
+        self.reader = shapefile.Reader(shp = myshp, dbf = mydbf)
+        self.west_lon = west_lon
+        self.east_lon = east_lon
+        self.south_lat = south_lat
+        self.north_lat = north_lat
+
+        self.shapes = self.reader.shapes()
+        self.records = self.reader.records()
+
+        self.length = len(self.shapes)
+
+
+    def bounding_box(self):
+        """
+        Serves as a container for the bounding coordinates of the area of interest
+        Returns 2 length list where each element is a four tuple
+        The first tuple defines longitudes, the second defines latitudes
+        """
+        first_point = self.shapes[0].points[0]
+        last_point = self.shapes[len(self.shapes)-1].points[0]
+
+        west_UTM = first_point[0]
+        east_UTM = last_point[0]
+
+        # Read the file to get UTM latitude bounds
+        north_UTM = first_point[1]
+        south_UTM = last_point[1]
+
+        return [(west_UTM, east_UTM, self.west_lon, self.east_lon), (south_UTM, north_UTM, self.south_lat, self.north_lat)]
+
+    def get_locations(self):
+        """
+        Returns list of (easting, northing) coordinate pairs
+        """
+
+        locs = []
+        for n in range(len(self.shapes)):
+            loc = self.shapes[n].points[0]
+            locs.append((loc[0], loc[1]))
+        return locs
+
+    def convert_to_lon_lat(self):
+        """
+        Takes in desired bounding lat/lon coordinates and remaps UTM coordinates to these
+        Returns two lists: latitudes, longitudes
+        """
+
+        raw = self.get_locations()
+
+# Make the list of four tuples to use as bounds in remap_interval
+        bounds = self.bounding_box()
+        lons = []
+        lats = []
+        for lon, lat in raw:
+            new_lon = remap_interval(lon, bounds[0][0], bounds[0][1], bounds[0][2], bounds[0][3])
+            new_lat = remap_interval(lat, bounds[1][0], bounds[1][1], bounds[1][2], bounds[1][3])
+            lons.append(new_lon)
+            lats.append(new_lat)
+        return lons, lats
+
+
+    def get_wind_speeds(self):
+        """
+        Returns two lists: the wind speed in m/s, and their directions
+        """
+        recs = self.records
+        speeds = []
+        directions = []
+
+        for n in range(len(recs)):
+            print(type(as_dict()))
+            speeds.append(self.records[n].as_dict()['speed'])
+            directions.append(self.records[n].as_dict()['QGIS_dir'])
+
+        return speeds, directions
+
+    def get_wind(self, input_lower_lon, input_upper_lon, input_lower_lat, input_upper_lat):
+        """
+        Only takes data that falls in target lat/lon range
+        """
+        lons, lats = self.convert_to_lon_lat()
+        speeds, dirs = self.get_wind_speeds()
+        wind_data = []
+        for n in range(len(lons)):
+            if lons[n] >= input_lower_lon and lons[n] <= input_upper_lon and lats[n] >= input_lower_lat and lats[n] <= input_upper_lat:
+                wind_data.append((speeds[n], dirs[n]))
+
+        return wind_data
+
+    def find_base_size(self):
+        """
+        Looks at the size of the UTM scale used in the .shp file to find out how
+        large each section is in meters. Also calculates the size of the rectangular
+        grid bounding the area.
+
+        Returns: grid size
+        """
+
+        first_UTM = self.shapes[0].points[0][0]
+        second_UTM = self.shapes[1].points[0][0]
+
+        grid_size = second_UTM - first_UTM
+
+        # Longitudinal grid dimension, latitudinal grid dimension:
+        # Divide the first and last lon/lat values by the size of the grid, add
+        # 1 to determine the total number of squares in each dimension
+        # lon_dim = (self.shapes[-1].points[0][0]-self.shapes[0].points[0][0]) / grid_size + 1
+        # lat_dim = (self.shapes[-1].points[0][1]-self.shapes[0].points[0][1]) / grid_size + 1
+
+        return grid_size
+
+    def regrid(self, new_size, input_lower_lon, input_upper_lon, input_lower_lat, input_upper_lat):
+        """
+        Takes an already calculated data set and projects it onto the desired size grid
+        """
+
+        old_size = self.find_base_size()
+
+        scaling_factor = old_size / new_size
+
+        wind_data = self.get_wind(input_lower_lon, input_upper_lon, input_lower_lat, input_upper_lat)
+
+
+        new_grid = [0] * int(len(wind_data)) * int(scaling_factor)
+
+        for id, el in enumerate(new_grid):
+            new_grid[id] = wind_data[int(id/scaling_factor)]
+
+        return new_grid
+
+
+# Everything that has to do with shapes
+
+wind = Wind(myshp, mydbf, west_lon, east_lon, south_lat, north_lat)
+print(wind.bounding_box())
+new_wind = wind.regrid(30, -120, -119.5, 37.5, 37.8)
+print(new_wind[-30:-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+#fucku
