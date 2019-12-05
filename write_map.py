d0d20798a5b79e298fcb1ba96a761b1ce345b5eb
Max Dietrich <maxcdietrich@gmail.com>
2019-12-05 18:09:41 -0500

    Add integration of get_wind.py
    Memory error still occurs in regrid() of get_wind.py when scaling the
    wind data to fit the simulation map.
    
    Added functions for making data informed maps in write_map.py.

--- write_map.py
+++ write_map.py
@@ -6,13 +6,14 @@ This file should create a Map object from the compiled data and then cache that 
 from random import randint
 import math
 import map
+import get_wind
 
 south_west_corner = (37.5, -120) #coordinate pair
 north_east_corner = (37.8, -119.5) #coordinate pair
-# x_length = 44190 #east-west length [m] ignore earth curvature
-# y_length = 33300 #north-south length [m]
-x_length = 15000 #Test values to make small map
-y_length = 15000 #Test values to make small map
+x_length = 44190 #east-west length [m] ignore earth curvature
+y_length = 33300 #north-south length [m]
+# x_length = 15000 #Test values to make small map
+# y_length = 15000 #Test values to make small map
 tile_size = 30 #GIS tile 30m x 30m
 
 
@@ -39,4 +40,27 @@ def write_random_map(filename):
     random_map = make_random_map()
     random_map.toJSON(filename)
 
-write_random_map('test_map')
+def make_real_map():
+    real_map = map.Map()
+    simulation_wind = get_wind.generate_wind() #get wind data
+    for y in range(y_length//tile_size):
+        for x in range(x_length//tile_size):
+            tile = map.Tile()
+            tile.is_burning = False
+            tile.flammability = randint(-40, 40) * math.cos(x /(300*math.pi))
+            tile.fuel = randint(-40, 40)
+            tile.wind = simulation_wind(y * (y_length // tile_size) + x) #convert 2d index to 1d index of wind_data
+            tile.elevation = math.sin(x/(100*math.pi)) * 50 * math.sin(y/(100*math.pi)) #1D sine wave
+            real_map.tile_dict[str((x, y))] = tile #key is a str because json does not take tuples as keys
+
+
+def write_real_map(filename):
+    """
+    Create a random map and write it to a text file in a JSON format
+    """
+    real_map = make_real_map()
+    real_map.toJSON(filename)
+
+
+# write_random_map('test_map')
+write_real_map('real_map')
